f60dae7e1a28f7f39766bfa93a2da012c6aeb6fc
jade <jadesada.jung@gmail.com>
2026-05-16 21:11:45 +0700

    set up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/login" element={<LoginPage />} />
+      <Route path="/movies" element={<ProtectedRoute><MoviesPage /></ProtectedRoute>} /> */}
+    </Routes>
+  )
+}
+
+export default App
