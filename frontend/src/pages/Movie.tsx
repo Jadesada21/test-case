7e045f168fdf7f5caac3f44b3d17aed3ea5b51df
jade <jadesada.jung@gmail.com>
2026-05-17 20:51:02 +0700

    movie page on process

--- frontend/src/pages/Movie.tsx
+++ frontend/src/pages/Movie.tsx
@@ -5,7 +5,7 @@ import Loading from '../components/loading'
 
 
 const MoviePage = observer(() => {
-    const { movieStore } = useStore()
+    const { movieStore, authStore } = useStore()
 
     useEffect(() => {
         movieStore.fetchMovies()
@@ -14,8 +14,16 @@ const MoviePage = observer(() => {
     if (movieStore.isLoading) return <Loading />
 
     return (
-        <div>
-            Hello Movie
+        <div className="min-h-screen flex justify-center items-center">
+            <div className="flex w-[70%] my-[15%] border border-white rounded-xl shadow-lg pt-8">
+                <div className="w-1/4 border-r border-white pl-4 pb-4">
+                    <p>menu bar</p>
+                </div>
+
+                <div className="w-3/4 pl-10 pb-4">
+                    <p>{authStore.user?.role} user name</p>
+                </div>
+            </div>
         </div>
     )
 })
