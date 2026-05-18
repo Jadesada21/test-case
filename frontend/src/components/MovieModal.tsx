2114f117bc510a17cdd8367375a17dc2e18f83d6
jade <jadesada.jung@gmail.com>
2026-05-18 15:43:12 +0700

    add movie modal

---
+++ frontend/src/components/MovieModal.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import { useStore } from '../stores'
+import type { MovieInput } from '../types/movie.type'
+import type { Props } from '../types/movie.modal.type'
+import Loading from './loading'
+
+
+
+export const MovieModal = observer(({ isOpen, onClose }: Props) => {
+    const { movieStore } = useStore()
+    const [form, setForm] = useState<MovieInput>({
+        title: "",
+        year_released: 0,
+        rating: ""
+    })
+
+    if (!isOpen) return null
+
+    const handleSubmit = async () => {
+        await movieStore.createMovies(form)
+        onClose()
+    }
+
+    return (
+
+        <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
+            <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
+                <h2 className="text-xl font-bold mb-4">Create Movie</h2>
+
+                <input
+                    type="text"
+                    placeholder='Title'
+                    value={form.title}
+                    onChange={e => setForm({ ...form, title: e.target.value })}
+                    maxLength={200}
+                    className="w-full border p-2 rounded mb-4"
+                />
+
+                <input
+                    type="text"
+                    placeholder='Year Released'
+                    value={form.year_released}
+                    onChange={e => setForm({ ...form, year_released: Number(e.target.value) })}
+                    maxLength={10}
+                    className="w-full border p-2 rounded mb-4"
+                />
+
+                <select
+                    value={form.rating}
+                    onChange={e => setForm({ ...form, rating: e.target.value })}
+                    className="w-full border p-2 rounded mb-4"
+                >
+                    <option value="">Selected Rating</option>
+                    <option value="G">G</option>
+                    <option value="PG">PG</option>
+                    <option value="M">M</option>
+                    <option value="MA">MA</option>
+                    <option value="R">R</option>
+                </select >
+
+                <div className="flex justify-end gap-4">
+                    <button
+                        onClick={handleSubmit}
+                        disabled={movieStore.isLoading}
+                        className="bg-blue-500 text-white p-2 rounded-xl px-4 disabled:opacity-50"
+                    >
+                        {movieStore.isLoading ? <Loading /> : 'created'}
+                    </button>
+
+                    <button
+                        onClick={onClose}
+                        className="border p-2 rounded-xl px-4">
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+})
--- frontend/src/pages/Movie.tsx
+++ frontend/src/pages/Movie.tsx
@@ -1,11 +1,15 @@
 import { observer } from "mobx-react-lite"
 import { useStore } from "../stores"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Loading from '../components/loading'
+import { Role } from "../types/user.type"
+import { MovieModal } from "../components/MovieModal"
 
 
 const MoviePage = observer(() => {
     const { movieStore, authStore } = useStore()
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     useEffect(() => {
         movieStore.fetchMovies()
@@ -13,17 +17,57 @@ const MoviePage = observer(() => {
 
     if (movieStore.isLoading) return <Loading />
 
+
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="flex w-[70%] my-[15%] border border-white rounded-xl shadow-lg pt-8">
-                <div className="w-1/4 border-r border-white pl-4 pb-4">
-                    <p>menu bar</p>
+                <div className="w-1/4 border-r border-white mb-4">
+                    <div className="flex justify-center pt-8">
+                        <p>Dashboard</p>
+                    </div>
                 </div>
 
-                <div className="w-3/4 pl-10 pb-4">
-                    <p>{authStore.user?.role} user name</p>
+                <div className="w-3/4 pl-10 pb-4 pr-8">
+                    <div className="flex justify-between items-center">
+                        <div className="font-bold">
+                            <p className="">Username: {authStore.user?.username} Manager1</p>
+                            <p className="">Role: {authStore.user?.role} Manager</p>
+                        </div>
+
+                        <button className="border p-3 rounded-2xl bg-red-400 font-bold">Logout</button>
+                    </div>
+
+                    <div className="pt-8">
+                        <button
+                            className="border p-4 rounded-2xl bg-green-700 font-bold"
+                            onClick={() => setIsModalOpen(true)}
+                        >
+                            Create Movie +
+                        </button>
+                    </div>
+
+                    {movieStore.movies.map(movie => (
+                        <div key={movie.id}>
+                            <p>{movie.title}</p>
+                            <p>{movie.rating}</p>
+                            <p>{movie.year_released}</p>
+                            <p>{movie.created_by.role}</p>
+
+                            {authStore.user?.role === Role.manager && (
+                                <button onClick={() => movieStore.deleteMovie(movie.id)}
+                                >
+                                    Delete
+                                </button>
+                            )}
+                        </div>
+                    ))}
                 </div>
             </div>
+
+            <MovieModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+            />
         </div>
     )
 })
